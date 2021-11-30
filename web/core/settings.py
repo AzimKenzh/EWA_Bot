28c8a965476c00a59d0889a61056598ddf532dad
Azim <azimkozho@gmail.com>
2021-11-30 12:20:29 +0600

    add cors

--- web/core/settings.py
+++ web/core/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'corsheaders',
-    # 'django_filters',
+    'corsheaders',
+    'django_filters',
 
     'drf_yasg',
     'rest_framework',
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -193,3 +194,5 @@ JET_THEMES = [
         'title': 'Light Gray'
     }
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
