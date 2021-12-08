2d9ddea2d6c71144966a4b6c0c666977b9cfb0a1
Azim <azimkozho@gmail.com>
2021-12-08 16:02:21 +0600

    remove filters, add search ebay, amazon

--- web/parsing/views.py
+++ web/parsing/views.py
@@ -5,11 +5,10 @@ import timeit
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from parsing.amazon import amazon_main
 from parsing.ebay import ebay_main
-from parsing.filters import EbayFilter, AmazonFilter
 from parsing.serializers import *
 from parsing.walmart import walmart_main
 
@@ -45,15 +44,15 @@ class AmazonAPIView(APIView):
 class EbayAdminViewSet(viewsets.ModelViewSet):
     queryset = Ebay.objects.all()
     serializer_class = EbaySerializerAdmin
-    filter_backends = [DjangoFilterBackend, ]
-    filter_class = EbayFilter
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title', ]
 
 
 class AmazonAdminViewSet(viewsets.ModelViewSet):
     queryset = Amazon.objects.all()
     serializer_class = AmazonSerializerAdmin
-    filter_backends = [DjangoFilterBackend, ]
-    filter_class = AmazonFilter
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['title', ]
 
 
 class WalmartAdminViewSet(viewsets.ModelViewSet):
