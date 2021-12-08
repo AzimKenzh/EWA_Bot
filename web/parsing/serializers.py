5855c84c9f7e83e862b0d7c676d98824a7d3dad1
Azim <azimkozho@gmail.com>
2021-12-08 18:35:27 +0600

    update

--- web/parsing/serializers.py
+++ web/parsing/serializers.py
@@ -56,11 +56,10 @@ class WalmartSerializerAdmin(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
 class ImportExcelSerializer(serializers.ModelSerializer):
     title = serializers.CharField(max_length=800, required=False)
 
     class Meta:
-        model = Walmart
+        model = ImportExcel
         fields = '__all__'
 
