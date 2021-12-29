977a37324f5950153d22f1e142101a6a3a6e47d8
Azim <azimkozho@gmail.com>
2021-12-29 16:54:12 +0600

    add start parser amazom

--- web/parsing/views.py
+++ web/parsing/views.py
@@ -35,7 +35,7 @@ class ProductTitleViewSet(viewsets.ModelViewSet):
 
         # start parse here
         ebay_main(instance)
-        # amazon_main(instance)
+        amazon_main(instance)
         instance.status = 'parsed'
         instance.save()
         return Response('OK')
@@ -50,7 +50,7 @@ class AllParseAPIView(APIView):
 
             # start parse here
             ebay_main(instance)
-            # amazon_main(instance)
+            amazon_main(instance)
             instance.status = 'parsed'
             instance.save()
 
