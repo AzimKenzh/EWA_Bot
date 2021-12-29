99eaf3808bae65c333581c62090bf2a39b28ab66
Azim <azimkozho@gmail.com>
2021-12-29 18:41:52 +0600

    add user agent in amazon

--- web/parsing/amazon.py
+++ web/parsing/amazon.py
@@ -11,11 +11,9 @@ from concurrent.futures import as_completed, ThreadPoolExecutor
 from parsing.models import Amazon
 
 headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
-    'Accept-Language': "en-gb",
-    'Accept-Encoding': 'br, gzip, deflate',
-    'Accept': 'test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Referer': 'https://www.google.com/'
+    'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'
 }
 
 
